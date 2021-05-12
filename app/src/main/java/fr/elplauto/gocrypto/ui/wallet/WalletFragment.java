a9124b0b6384e1c0bca22819656a9e2dc23f1acf
Elplauto <florian.plaut.etu@univ-lemans.fr>
2021-05-12 10:29:52 +0200

    wallet chart

--- app/src/main/java/fr/elplauto/gocrypto/ui/wallet/WalletFragment.java
+++ app/src/main/java/fr/elplauto/gocrypto/ui/wallet/WalletFragment.java
@@ -1,5 +1,6 @@
 package fr.elplauto.gocrypto.ui.wallet;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,9 +10,25 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+import java.text.Format;
+import java.util.ArrayList;
+import java.util.List;
 
 import fr.elplauto.gocrypto.R;
 import fr.elplauto.gocrypto.database.DBManager;
@@ -22,32 +39,99 @@ import fr.elplauto.gocrypto.model.Wallet;
 public class WalletFragment extends Fragment {
 
     private static final String TAG = "WalletFragment";
-    private WalletViewModel walletViewModel;
     private DBManager dbManager;
+    LineChart chart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        walletViewModel =
-                ViewModelProviders.of(this).get(WalletViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_wallet, container, false);
-        final TextView textView = root.findViewById(R.id.text_wallet);
-        walletViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        chart = (LineChart) root.findViewById(R.id.chartWallet);
 
         dbManager = new DBManager(getContext());
-        Wallet wallet = new Wallet(351.23);
-        wallet.getHistory().add(new History("lalala", 985.100));
-        wallet.getHistory().add(new History("papapa", 1000.100));
-        wallet.getCrypto().add(new CryptoInWallet(1, 468.2, 453.3));
-        wallet.getCrypto().add(new CryptoInWallet(2, 468.2, 453.3));
-        dbManager.saveWallet(wallet);
-        Wallet test = dbManager.getWallet();
-        Log.d(TAG, wallet.getUsd()+"");
-
+        Wallet wallet = dbManager.getWallet();
+        drawChart(wallet.getHistory());
+        
         return root;
+    }
+
+    private void drawChart(List<History> historyList) {
+        List <Entry> entries = new ArrayList<>();
+        int index = 0;
+        for (History history : historyList) {
+            entries.add(new Entry(index, history.getValue().floatValue()));
+            index++;
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setValueFormatter(new MyValueFormatter(entries));
+        dataSet.setCircleColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        dataSet.setColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+        LineData lineData = new LineData(dataSet);
+        lineData.setValueTextColor(Color.BLACK);
+        lineData.setValueTextSize(10f);
+        chart.setData(lineData);
+        chart.getDescription().setEnabled(false);
+        chart.setTouchEnabled(false);
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawLabels(false);
+        xAxis.setDrawGridLines(false);
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setDrawAxisLine(false);
+        leftAxis.setDrawLabels(false);
+        leftAxis.setDrawGridLines(false);
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setDrawAxisLine(false);
+        rightAxis.setDrawLabels(false);
+        rightAxis.setDrawGridLines(false);
+        chart.getLegend().setEnabled(false);
+        chart.invalidate(); // refresh
+    }
+
+
+
+
+}
+
+class MyValueFormatter extends ValueFormatter {
+
+    private List<Entry> entries;
+    private Entry maxEntry;
+    private Entry minEntry;
+
+    public MyValueFormatter(List<Entry> entries) {
+        this.entries = entries;
+        this.maxEntry = getMaxEntry();
+        this.minEntry = getMinEntry();
+    }
+
+    public Entry getMaxEntry() {
+        Entry max = this.entries.get(0);
+        for (Entry entry : this.entries) {
+            if (entry.getY() > max.getY()) {
+                max = entry;
+            }
+        }
+        return max;
+    }
+
+    public Entry getMinEntry() {
+        Entry min = this.entries.get(0);
+        for (Entry entry : this.entries) {
+            if (entry.getY() < min.getY()) {
+                min = entry;
+            }
+        }
+        return min;
+    }
+
+    @Override
+    public String getPointLabel(Entry entry) {
+        if (entry.equals(maxEntry) || entry.equals(minEntry)) {
+            return entry.getY()+"";
+        } else {
+            return "";
+        }
     }
 }
